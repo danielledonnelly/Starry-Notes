3a3fb25607c1036db4db5e98c90bbd2ebe8412ba
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-07-24 20:38:58 -0230

    basic setup complete

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import { useRef, useEffect } from "react";
+import React from "react";
+import Trash from "../icons/Trash";
 
-export const NoteCard = ({note}) => {
-  const body = JSON.parse(note.body)
-  const position = JSON.parse(note.position)
+export const NoteCard = ({ note }) => {
+  const body = JSON.parse(note.body);
+  const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
+
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textarea) => {
+    const { current } = textAreaRef;
+    current.style.height = "auto"; // Reset the height
+    current.style.height = current.scrollHeight + "px"; // Set the new height
+  };
+
   return (
-    <div className="card" style={{backgroundColor: colors.colorBody}}>
-      {body}
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      >
+        <Trash />
+      </div>
+      <div className="card-body">
+        <textarea
+          ref={textAreaRef}
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+          onInput={() => {autoGrow(textAreaRef)}}
+        ></textarea>
+      </div>
     </div>
-  )
-}
+  );
+};
