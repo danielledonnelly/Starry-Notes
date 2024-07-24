20e3a930a708694be6c3f52fd4f472ce3c62921d
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-07-24 20:52:24 -0230

    mouse moving

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,11 +1,14 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import React from "react";
 import Trash from "../icons/Trash";
 
 export const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
-  const position = JSON.parse(note.position);
+  const [position, setPosition] = useState(JSON.parse(note.position));
   const colors = JSON.parse(note.colors);
+  
+  let mouseStartPos = { x: 0, y: 0 };
+  const cardRef = useRef(null);
 
   const textAreaRef = useRef(null);
 
@@ -19,8 +22,28 @@ export const NoteCard = ({ note }) => {
     current.style.height = current.scrollHeight + "px"; // Set the new height
   };
 
+  const mouseDown = (e) => {
+    mouseStartPos.x = e.clientX,
+    mouseStartPos.y = e.clientY
+
+  document.addEventListener('mousemove', mouseMove)
+  }
+
+  const mouseMove = (e) => {
+    const mouseMoveDir = {
+      x: mouseStartPos.x - e.clientX,
+      y: mouseStartPos.y - e.clientY
+    };
+
+    console.log('mouseMoveDir:', mouseMoveDir);
+
+    mouseStartPos.x = e.clientX,
+    mouseStartPos.y = e.clientY
+  } 
+
   return (
     <div
+      ref={cardRef}
       className="card"
       style={{
         backgroundColor: colors.colorBody,
@@ -29,6 +52,7 @@ export const NoteCard = ({ note }) => {
       }}
     >
       <div
+        onMouseDown={mouseDown}
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
       >
