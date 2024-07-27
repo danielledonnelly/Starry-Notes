af992dd469450ca6136b0f60c1a752072d8e9e01
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-07-27 11:41:56 -0230

    sun button fixed

--- src/icons/Plus.jsx
+++ src/icons/Plus.jsx
@@ -1,14 +1,13 @@
-const Plus = ({ size = "24", color = "#FFFFFF", className }) => {
+const Plus = ({ size = "22", color = "#FFFFFF", className }) => {
     return (
         <svg
             className={className} // Add this line
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
+            viewBox="0 0 22 22"
             width={size}
             height={size}
             stroke={color}
             fill="none"
-            strokeWidth="2.5"
         >
             <path strokeLinecap="round" d="M18 12H6M12 6v12"></path>
         </svg>
