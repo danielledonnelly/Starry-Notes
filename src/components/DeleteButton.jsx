8dacc1280999041c31b7fe1ccdcdb28c076d8581
danielledonnelly <daniellemarydonnelly@gmail.com>
2024-07-27 11:26:38 -0230

    fast trash

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,25 +1,23 @@
-// import Trash from "../icons/Trash";
+import Trash from "../icons/Trash";
 import { db } from "../appwrite/database";
 import { useContext } from "react";
 import { NotesContext } from "../context/NoteContext";
-import XIcon from '../icons/x.svg';
-
+import Icon from '../icons/x.svg';
 // import { NotesContext } from "../context/NotesContext";
 
 const DeleteButton = ({ noteId }) => {
+//  Commented this out because for some reason it triggers constantly otherwise
+//  console.log("Delete clicked")
     const { setNotes } = useContext(NotesContext);
-
     const handleDelete = async (e) => {
-        e.stopPropagation();
-        await db.notes.delete(noteId);
+        db.notes.delete(noteId);
         setNotes((prevState) =>
             prevState.filter((note) => note.$id !== noteId)
         );
     };
-
     return (
         <div onClick={handleDelete}>
-            <img src={XIcon} alt="Delete" className="delete-icon" />
+            <Trash />
         </div>
     );
 };
